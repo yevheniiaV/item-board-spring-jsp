11f8c2f6153d27f4a9baba26c88a1869652985db
YevheniiaV <yevheniia.vorobei@gmail.com>
2017-10-05 18:04:12 +0300

    Logging registration controller

--- src/main/java/controller/RegistrationController.java
+++ src/main/java/controller/RegistrationController.java
@@ -3,6 +3,7 @@ package controller;
 import dao.UserDAO;
 import dao.UserDAOService;
 import entity.User;
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -18,35 +19,37 @@ import java.io.IOException;
 
 @Controller
 public class RegistrationController {
+    private static final Logger log = Logger.getLogger(LoginController.class);
+    UserDAOService userDAOService = new UserDAOService(new UserDAO());
 
     @ModelAttribute("user")
     public User createModel() {
         return new User();
     }
 
-    UserDAOService userDAOService = new UserDAOService(new UserDAO());
-
     private void registerUser(User user, HttpSession session, String login){
         userDAOService.add(user);
         session.setAttribute("login", login);
+        log.info("User "+ login + " was registrated successfully!");
     }
 
-    @RequestMapping(value = "/register", method = RequestMethod.POST)
+    @RequestMapping(value = "/register.html", method = RequestMethod.POST)
     public String login(@Valid User user, BindingResult result, HttpSession session, HttpServletRequest request, HttpServletResponse response, Model model, String email, String name, String pass, String pass2, String phone, String city) throws IOException {
-
         if (result.hasErrors()){
             if(!pass.equals(pass2)){
-                System.out.println(pass);
-                System.out.println(pass2);
-                model.addAttribute("passwordsNotEqualsError", "olol");
-                    return "account";
+                log.info("User "+ user.getName() + " was not registrated! Passwords not equal");
+                return "account";
                 }else {
                     return "account";}
-        }else if((userDAOService.getByLogin(name) == null) && (pass.equals(pass2)) && (!(name.equals("")) && !(email.equals("")) && !(pass.equals("")) && !(pass2.equals("")))) {
-            user = new User(email, name, pass, phone, city);
+        } else if((userDAOService.getByEmail(email) == null) && (pass.equals(pass2))) {
             registerUser(user, session, name);
             response.sendRedirect("/");
             return "main";
-        } return null;
+        } else if (!(userDAOService.getByEmail(email) == null)){
+            model.addAttribute("userExistError", "User exists!");
+            log.info("User "+ user.getName() + " was not registrated! User exists!");
+            return "account";
+        }
+        return null;
     }
 }
