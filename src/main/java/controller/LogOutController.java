eb229f9f03c0c6de596323cf18c8b5448ca783ba
YevheniiaV <yevheniia.vorobei@gmail.com>
2017-10-05 14:11:07 +0300

    Logging logout Controller

--- src/main/java/controller/LogOutController.java
+++ src/main/java/controller/LogOutController.java
@@ -1,5 +1,6 @@
 package controller;
 
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -10,10 +11,13 @@ import java.io.IOException;
 
 @Controller
 public class LogOutController {
+    private static final Logger log = Logger.getLogger(LoginController.class);
 
     @RequestMapping("/logout.html")
     private String goToMainPage(HttpSession session, HttpServletResponse response) throws IOException {
-        if (session.getAttribute("login") != null) {
+        String login = session.getAttribute("login").toString();
+        if ( login != null) {
+            log.info("User "+ login + " was logged out!");
             session.removeAttribute("login");
         }
         try {
