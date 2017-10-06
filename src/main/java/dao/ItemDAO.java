75c4e557f1c090e00df476ce078ccf243d9050b1
YevheniiaV <yevheniia.vorobei@gmail.com>
2017-10-06 11:17:58 +0300

    Geting items list from db via ItemDao method

---
+++ src/main/java/dao/ItemDAO.java
@@ -0,0 +1,22 @@
+package dao;
+
+import entity.Item;
+import hibernate.HibernateUtil;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+import java.util.List;
+
+public class ItemDAO {
+
+    List<Item> get(){
+        SessionFactory sf = HibernateUtil.getSessionFactory();
+        Session s = sf.openSession();
+        Query q = s.createQuery("FROM Item");
+        List<Item> out = q.list();
+        s.close();
+        return out;
+    }
+
+}
